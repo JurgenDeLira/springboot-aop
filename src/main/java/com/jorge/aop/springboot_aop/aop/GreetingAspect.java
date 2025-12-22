54b4f3cd8ec801b25c962304d63cae76d0ab7c6f
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-12-21 17:50:34 -0700

    Advice After

--- src/main/java/com/jorge/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/jorge/aop/springboot_aop/aop/GreetingAspect.java
@@ -1,6 +1,7 @@
 package com.jorge.aop.springboot_aop.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -15,11 +16,19 @@ public class GreetingAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* com.jorge.aop.springboot_aop.services.GreetingService.sayHello(..))")
+    @Before("execution(* com.jorge.aop.springboot_aop.services.GreetingService.*(..))")
     public void loggerBefore(JoinPoint joinPoint){
 
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Antes: " + method + " con los argumentos " + args);
+    }
+
+    @After("execution(* com.jorge.aop.springboot_aop.services.GreetingService.*(..))")
+    public void loggerAfter(JoinPoint joinPoint){
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Después: " + method + " con los argumentos " + args);
     }
 }
